27dd47fa62b4b77dd6aa9b72bda095f8fa8f80ba
Andriinef <andrii.nefedov@nure.ua>
2022-11-04 12:09:27 +0200

    class color

--- lesson_05/homework_05_01.py
+++ lesson_05/homework_05_01.py
@@ -1,35 +1,36 @@
 from colors import Colors
 
-""" Создайте контекстный менеджер colorizer,
+
+"""
+    Создайте контекстный менеджер colorizer,
     который будет печатать заданным цветом в произвольном блоке кода.
     После выхода из блока текст печатается обычным образом
 """
 
-color = {
-    "ENDC": "\033[0m",
-    "grey": "\033[90m",
-    "green": "\033[92m",
-    "yellow": "\033[93m",
-    "blue": "\033[94m",
-    "pink": "\033[95m",
-    "red": "\033[91m",
-    "turquoise": "\033[96m",
-}
-
-
 class Colorizer:
+    color = {
+        "grey": "\033[90m",
+        "red": "\033[91m",
+        "green": "\033[92m",
+        "yellow": "\033[93m",
+        "blue": "\033[94m",
+        "pink": "\033[95m",
+        "turquoise": "\033[96m",
+        "ENDC": "\033[0m",
+    }
+
     def __init__(self, colorizer):
-        if colorizer in color:
-            self.color = colorizer
-        else:
-            print(f"{Colors.FAIL}Error color")
-            self.color = "ENDC"
+        self.color = colorizer
 
     def __enter__(self):
-        for key, value in color.items():
-            if key == self.color:
-                break
-        print(f"{value}", end="")
+        if self.color in Colorizer.color:
+            for key, value in Colorizer.color.items():
+                if key == self.color:
+                    break
+            print(f"{value}", end="")
+        else:
+            print(f"{Colors.FAIL}Error color")
+            self.color = Colorizer.color["ENDC"]
 
     def __exit__(self, type, value, traceback):
         print(f"{Colors.ENDC}", end="")
