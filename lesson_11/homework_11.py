e33b51d8794c9226cee4e12735e1350679381c3a
Andriinef <andrii.nefedov@nure.ua>
2022-12-03 23:27:59 +0200

    new_homework_11

--- lesson_11/homework_11.py
+++ lesson_11/homework_11.py
@@ -2,36 +2,38 @@ from threading import Thread
 from time import sleep
 
 
-def get_primes(start: int, end: int) -> list[int]:
+def get_primes(start: int, end: int, thread: int) -> list[int]:
+    _range = int(end / thread)
+    _start = start
+    _end = _range
     results = []
-    for number in range(start, end + 1):
-        prime = True
-        for i in range(2, number):
-            if number % i == 0:
-                prime = False
-                break
-        if END > number:
-            if prime:
-                results.append(number)
+
+    def inner(results, _start: int, _end: int, item: int):
+        _results = []
+        for number in range(_start, _end + 1):
+            prime = True
+            for i in range(2, number):
+                if number % i == 0:
+                    prime = False
+                    break
+            if end > number:
+                if prime:
+                    _results.append(number)
+        print(f"Thread {item+1}: {_results}")
+        results.extend(_results)
+
+    for item in range(thread):
+        if end > _start:
+            _thread = Thread(target=inner, args=[results, _start, _end, item])
+            _thread.start()
+            sleep(1)
+            _start = _end + 1
+            _end += _range
+            _thread.join()
     return results
 
 
-def foo(start: int, end: int):
-    sleep(1)
-    data = get_primes(start, end)
-    print(f"Thread {item+1}: {data}")
-
-
 if __name__ == "__main__":
-    start = 100
-    END = 10000
-    thread_ = 5
-    range_ = int(END / thread_)
-    end = range_
-    for item in range(thread_):
-        if END > start:
-            thread = Thread(target=foo, args=[start, end])
-            thread.start()
-            start = end + 1
-            end += range_
-            thread.join()
+    thread = 5
+    data = get_primes(1, 100, thread)
+    print(data)
